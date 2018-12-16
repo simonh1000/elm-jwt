c24cb1ae04feea53ee13255acd4801f4cb2ddad4
Simon Hampton <hotbelgo@gmail.com>
2018-12-16 12:26:49 +0100

    update dev env

--- examples/node/README.md
+++ examples/node/README.md
@@ -1,30 +1,92 @@
-# Elm-Jwt: Node example
+# Elm 0.19 with Webpack 4, Hot Reloading & Babel 7
+
+Elm dev environment with hot-loading (i.e. state is retained as you edit your code - Hot Module Reloading, HMR)). I use this daily for my professional work. Like elm-community/elm-webpack-starter but using Webpack 4.
 
 ## Installation
 
-elm make src/Main.elm --output=index019.html
-http://localhost:5000/index019.html
-
-
--- Too be updated
+Clone this repo into a new project folder and run install script.
+(You will probably want to delete the .git/ directory and start version control afresh.)
 
 With npm
 
 ```sh
-npm install
-npm run server
-<in another window>
-npm run dev
+$ git clone git@github.com:simonh1000/elm-webpack-starter.git new-project
+$ cd new-project
+$ npm install
+$ npm run dev
 ```
-
 
 With yarn
 ```sh
-$ yarn install
-$ yarn server
-<in another window>
+$ git clone git@github.com:simonh1000/elm-webpack-starter.git new-project
+$ cd new-project
+$ yarn
 $ yarn dev
  ```
 
 Open http://localhost:3000 and start modifying the code in /src.
 (An example using Routing is provided in the `navigation` branch)
+
+## Production
+
+Build production assets (js and css together) with:
+
+```sh
+npm run prod
+```
+
+## Static assets
+
+Just add to `src/assets/` and the production build copies them to `/dist`
+
+## Testing
+
+[Install elm-test globally](https://github.com/elm-community/elm-test#running-tests-locally)
+
+`elm-test init` is run when you install your dependencies. After that all you need to do to run the tests is
+
+```
+yarn test
+```
+
+Take a look at the examples in `tests/`
+
+If you add dependencies to your main app, then run `elm-test --add-dependencies`
+
+<!-- I have also added [elm-verify-examples](https://github.com/stoeffel/elm-verify-examples) and provided an example in the definition of `add1` in App.elm. -->
+
+<hr />
+
+## ES6
+
+This starter includes [Babel](https://babeljs.io/) so you can directly use ES6 code.
+
+## Changelog
+
+ - 3.2.0 - Add elm-minify to prod builds (thanks Asger)
+ - 3.1.0 - Revert to webpack-dev-server with example
+ - 3.0.2 - bugfixes (mostly for tests)
+ - 3.0.1 - use publicPath to ensure compatibility with more complex routes
+ - 3.0.0 - version 0.19
+ - 2.1.0 - switch to webpack-serve (from webpack dev server)
+ - 2.0.0
+    - Remove Bootstrap (use purecss as simple alternative - you don't want me choosing your css framework after all)
+    - Compile CSS into separate file
+ - 1.2.0 - Webpack 4, Babel 7
+ - 1.1.0 - add elm-verify-examples
+
+ ## Credits
+
+ A long time ago this code was forked from https://github.com/fluxxu/elm-hot-loader
+
+ ## How it works
+
+ ```
+ webpack-serve --hot --colors --port 3000
+ webpack-serve --hot --host=0.0.0.0 --port 3000
+ ```
+
+  - `--hot` Enable webpack's Hot Module Replacement feature
+  - `--host=0.0.0.0` - enable you to reach your dev environment from another device - e.g  your phone
+  - `--port 3000` - use port 3000 instead of default 8000
+  - inline (default) a script will be inserted in your bundle to take care of reloading, and build messages will appear in the browser console.
--- examples/node/webpack.config.js
+++ examples/node/webpack.config.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const history = require('koa-connect-history-api-fallback');
-const convert = require('koa-connect');
-const proxy = require('http-proxy-middleware');
+const elmMinify = require("elm-minify");
 
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
@@ -12,7 +10,8 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 // to extract the css as a separate file
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-var MODE = process.env.npm_lifecycle_event === "prod" ? "production" : "development";
+var MODE =
+    process.env.npm_lifecycle_event === "prod" ? "production" : "development";
 var filename = MODE == "production" ? "[name]-[hash].js" : "index.js";
 
 var common = {
@@ -20,6 +19,7 @@ var common = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "dist"),
+        publicPath: "/",
         // webpack -p automatically adds hash when building for production
         filename: filename
     },
@@ -47,12 +47,13 @@ var common = {
             {
                 test: /\.scss$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loaders: ["style-loader", "css-loader", "sass-loader"]
+                // see https://github.com/webpack-contrib/css-loader#url
+                loaders: ["style-loader", "css-loader?url=false", "sass-loader"]
             },
             {
                 test: /\.css$/,
                 exclude: [/elm-stuff/, /node_modules/],
-                loaders: ["style-loader", "css-loader"]
+                loaders: ["style-loader", "css-loader?url=false"]
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -70,6 +71,7 @@ var common = {
             },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i,
+                exclude: [/elm-stuff/, /node_modules/],
                 loader: "file-loader"
             }
         ]
@@ -91,13 +93,11 @@ if (MODE === "development") {
                     test: /\.elm$/,
                     exclude: [/elm-stuff/, /node_modules/],
                     use: [
-                        {
-                            loader: "elm-hot-loader"
-                        },
+                        { loader: "elm-hot-webpack-loader" },
                         {
                             loader: "elm-webpack-loader",
-                            // add Elm's debug overlay to output
                             options: {
+                                // add Elm's debug overlay to output
                                 debug: true,
                                 forceWatch: true
                             }
@@ -106,31 +106,35 @@ if (MODE === "development") {
                 }
             ]
         },
-        serve: {
+        devServer: {
             inline: true,
             stats: "errors-only",
-            content: [path.join(__dirname, "src/assets")],
-            add: (app, middleware, options) => {
-                // routes /xyz -> /index.html
-                app.use(history());
-                app.use(convert(proxy('/api', { target: 'http://localhost:5000' })));
-                app.use(convert(proxy('/sessions', { target: 'http://localhost:5000' })));
+            contentBase: path.join(__dirname, "src/assets"),
+            historyApiFallback: true,
+            // feel free to delete this section if you don't need anything like this
+            before(app) {
+                // on port 3000
+                app.get("/test", function(req, res) {
+                    res.json({ result: "OK" });
+                });
             }
         }
     });
 }
-
 if (MODE === "production") {
     console.log("Building for Production...");
     module.exports = merge(common, {
         plugins: [
-            // Delete everything from output directory and report to user
+            // Minify elm code
+            new elmMinify.WebpackPlugin(),
+            // Delete everything from /dist directory and report to user
             new CleanWebpackPlugin(["dist"], {
                 root: __dirname,
                 exclude: [],
                 verbose: true,
                 dry: false
             }),
+            // Copy static assets
             new CopyWebpackPlugin([
                 {
                     from: "src/assets"
@@ -147,22 +151,30 @@ if (MODE === "production") {
                 {
                     test: /\.elm$/,
                     exclude: [/elm-stuff/, /node_modules/],
-                    use: [
-                        {
-                            loader: "elm-webpack-loader"
+                    use: {
+                        loader: "elm-webpack-loader",
+                        options: {
+                            optimize: true
                         }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    exclude: [/elm-stuff/, /node_modules/],
+                    loaders: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader?url=false"
                     ]
                 },
-            {
-                test: /\.css$/,
-                exclude: [/elm-stuff/, /node_modules/],
-                loaders: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-            {
-                test: /\.scss$/,
-                exclude: [/elm-stuff/, /node_modules/],
-                loaders: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            }
+                {
+                    test: /\.scss$/,
+                    exclude: [/elm-stuff/, /node_modules/],
+                    loaders: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader?url=false",
+                        "sass-loader"
+                    ]
+                }
             ]
         }
     });
